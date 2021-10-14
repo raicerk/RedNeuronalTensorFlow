1056ef1bf08e4b0c56abfe2d653dad8388a9a833
Juan Mora <juanvalentin.mora@globant.com>
2021-10-13 23:35:14 -0300

    Se modifican nombres de variables para estandarización de código

--- multiplos_de_dos.py
+++ multiplos_de_dos.py
@@ -4,10 +4,10 @@ import numpy as np
 numeros = np.array([1,2,3,4,5,6,7,8,9,10], dtype=int)
 multiplo_de_dos = np.array([2,4,6,8,10,12,14,16,18,20], dtype=int)
 
-entrada = tf.keras.layers.Dense(units=3, input_shape=[1])
-neurona1 = tf.keras.layers.Dense(units=3)
-salida = tf.keras.layers.Dense(units=1)
-modelo = tf.keras.Sequential([entrada, neurona1, salida])
+capa_entrada = tf.keras.layers.Dense(units=3, input_shape=[1])
+capa_oculta = tf.keras.layers.Dense(units=3)
+capa_salida = tf.keras.layers.Dense(units=1)
+modelo = tf.keras.Sequential([capa_entrada, capa_oculta, capa_oculta, capa_salida])
 
 modelo.compile(
     optimizer = tf.keras.optimizers.Adam(0.1),
@@ -15,13 +15,12 @@ modelo.compile(
 )
 
 print("Comenzando entrenamiento...")
-historial = modelo.fit(numeros, multiplo_de_dos, epochs=1000, verbose=False)
+historial = modelo.fit(numeros, multiplo_de_dos, epochs=300, verbose=False)
 print("Modelo entrenado!")
 
 print("Hagamos una prediccion de multiplo de dos!")
-resultado = np.round(modelo.predict([100, 200, 111, 1325]), decimals=1)
-print(type(resultado))
-print("El resultado es " +str(resultado))
+resultado = np.around(modelo.predict([100, 200, 111, 1325]), decimals=1).astype(int)
+print("El resultado es " + str(resultado))
 
 print("Variables internas del modelo")
-print(entrada.get_weights())
+print(capa_entrada.get_weights())
