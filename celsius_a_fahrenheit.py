c00c9242c68af40e9eb431f892dc8368898d8869
Juan Mora <juanvalentin.mora@globant.com>
2021-10-21 23:11:58 -0300

    se agrega grafico para entrenamiento lineal

--- celsius_a_fahrenheit.py
+++ celsius_a_fahrenheit.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 celsius = np.array([-40,-10,0,8,15,22,38], dtype=float)
 fahrenheit = np.array([-40,14,32,46,59,72,100], dtype=float)
@@ -15,6 +16,12 @@ modelo.compile(
 print("Comenzando entrenamiento...")
 historial = modelo.fit(celsius, fahrenheit, epochs=1000, verbose=False)
 print("Modelo entrenado!")
+
+# Visor del entrenamiento
+plt.xlabel("# Epoca")
+plt.ylabel("Magnitud de pérdida")
+plt.plot(historial.history["loss"])
+plt.show()
 
 print("Hagamos una prediccion de temperatura de celsius a fahrenheit!")
 resultado = modelo.predict([100.0])
